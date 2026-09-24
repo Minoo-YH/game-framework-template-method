70d22779efb21612c0c3b394dd5474819663db13
Minoo Yaghoubi <155952014+Minoo-YH@users.noreply.github.com>
2026-09-25 00:11:39 +0300

    Implement DiceGame using Template Method pattern

--- src/main/java/org/example/DiceGame.java
+++ src/main/java/org/example/DiceGame.java
@@ -1,4 +1,57 @@
 package org.example;
+// extends means that DiceGame inherits from Game
+//scores stores the scores of all players.
 
-public class DiceGame {
+
+import java.util.Random;
+
+public class DiceGame extends Game {
+
+    private int[] scores;
+    private int winningPlayer = -1;
+    private final int winningScore = 20;
+
+    private final Random random = new Random();
+
+    @Override
+    public void initializeGame(int numberOfPlayers) {
+        scores = new int[numberOfPlayers];
+        winningPlayer = -1;
+
+        System.out.println("Dice Game started!");
+        System.out.println("First player to reach "
+                + winningScore + " points wins.");
+        System.out.println();
+    }
+
+    @Override
+    public boolean endOfGame() {
+        return winningPlayer != -1;
+    }
+
+    @Override
+    public void playSingleTurn(int player) {
+
+        int dice = random.nextInt(6) + 1;
+
+        scores[player] = scores[player] + dice;
+
+        System.out.println(
+                "Player " + (player + 1)
+                        + " rolled " + dice
+                        + " | Score: " + scores[player]
+        );
+
+        if (scores[player] >= winningScore) {
+            winningPlayer = player;
+        }
+    }
+
+    @Override
+    public void displayWinner() {
+        System.out.println();
+        System.out.println(
+                "Player " + (winningPlayer + 1) + " wins!"
+        );
+    }
 }
